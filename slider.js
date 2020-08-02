c89e51b9659c1adef68e4df5a76ed43c5960a584
Mariam <mariam.manvelidze.1@btu.edu.ge>
2020-08-02 23:03:54 +0400

    add: review section, slick slider

---
+++ slider.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.review-slider').slick({
+        infinite: false,
+        slidesToShow:3,
+        slidesToScroll:1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed:1000,
+    });
+});
